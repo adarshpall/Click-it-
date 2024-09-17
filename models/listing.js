50f2e9e2925cc71966cefd218c1f6241c89dfd59
adarshpall <ap2724875@gmail.com>
2024-09-17 12:40:10 +0530

    models edited

--- models/listing.js
+++ models/listing.js
@@ -1,22 +1,25 @@
-const mongoose =require ("mongoose");
-const Schema =mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const listingSchema=new Schema({
-    title:{
-        type: String,
-        required:true,
-    },
-    description : String,
-    image :{
-        type :String,
-        default:"https://in.images.search.yahoo.com/search/images;_ylt=AwrKEatXCtxmLAQAqQ.7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3BpdnM-?p=hotel+pics&fr2=piv-web&type=E210IN885G0&fr=mcafee#id=0&iurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F1134176%2Fpexels-photo-1134176.jpeg%3Fcs%3Dsrgb%26dl%3Ddug-out-pool-hotel-pool-1134176.jpg%26fm%3Djpg&action=click",
-        set: (v)=>
-            v===""?"https://in.images.search.yahoo.com/search/images;_ylt=AwrKEatXCtxmLAQAqQ.7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3BpdnM-?p=hotel+pics&fr2=piv-web&type=E210IN885G0&fr=mcafee#id=0&iurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F1134176%2Fpexels-photo-1134176.jpeg%3Fcs%3Dsrgb%26dl%3Ddug-out-pool-hotel-pool-1134176.jpg%26fm%3Djpg&action=click":v,
-    },
-    price: Number,
-    location:String,
-    state:String,
-    
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
-const Listing =mongoose.model("Listing",listingSchema);
-module.exports=Listing;
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
