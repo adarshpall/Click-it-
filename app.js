f5e0d0ada4ecf6ad64b2dd5a7a3a6032c1359176
adarshpall <ap2724875@gmail.com>
2024-09-24 13:38:58 +0530

    joi used

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-
+const {listingSchema} = require("./schema.js")
 main()
   .then(() => {
     console.log("connected to DB");
@@ -50,28 +50,32 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 }));
 
 //Create Route
-app.post("/listings",wrapAsync( async (req, res) => {
+app.post("/listings",wrapAsync( async (req, res,next) => {
+ let result= listingSchema.validate(req.body);
+ if(result.error){
+  throw new ExpressError(404,result.error);
+ }
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
 }));
 
 //Edit Route
-app.get("/listings/:id/edit",wrapAsync( async (req, res) => {
+app.get("/listings/:id/edit",wrapAsync( async (req, res,next) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/edit.ejs", { listing });
 }));
 
 //Update Route
-app.put("/listings/:id", wrapAsync(async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res,next) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
 }));
 
 //Delete Route
-app.delete("/listings/:id",wrapAsync( async (req, res) => {
+app.delete("/listings/:id",wrapAsync( async (req, res,next) => {
   let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
